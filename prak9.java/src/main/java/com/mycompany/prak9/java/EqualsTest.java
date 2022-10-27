37060e061d60060bc1f4462294d33bf74df04254
Musfarazn <112687844+Musfarazn@users.noreply.github.com>
2022-10-27 18:35:21 +0700

    Add files via upload

---
+++ prak9.java/src/main/java/com/mycompany/prak9/java/EqualsTest.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.prak9.java;
+
+/**
+ *
+ * @author MUSFARA ZAHRA NADIEN
+ */
+public class EqualsTest {
+
+    public static void main(String[] args) {
+        String str1, str2;
+        str1 = "Free the bound periodicals";
+        
+        str2 = str1;
+        System.out.println("String 1 : " + str1);
+        System.out.println("String 2 : " + str2);
+        System.out.println("Same object? " + (str1 == str2));
+        
+        str2 = new String(str1);
+        System.out.println("String 1 : " + str1);
+        System.out.println("String 2 : " + str2);
+        System.out.println("Same object? " + (str1 == str2));
+        
+        System.out.println("Same value? " + str1.equals (str2));
+    }
+}
